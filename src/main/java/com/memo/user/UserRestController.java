909222dba374fe3825d4f4fc7d73ced7beecc458
megastudyDESKTOP30 <test@test.com>
2024-01-15 18:44:30 +0900

    1/ 15

--- src/main/java/com/memo/user/UserRestController.java
+++ src/main/java/com/memo/user/UserRestController.java
@@ -3,25 +3,99 @@ package com.memo.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.memo.common.EncryptUtils;
+import com.memo.user.bo.UserBO;
+import com.memo.user.entity.UserEntity;
 
 @RequestMapping("/user")
 @RestController
 public class UserRestController {
 
+	@Autowired
+	private UserBO userBO;
+	
+	/*
+	 * 아이디 중복확인 API
+	 * 
+	 */
+	
 	@RequestMapping("/is-duplicated-id")
 	public Map<String, Object> isDuplicatedId(
 			@RequestParam("loginId") String loginId) {
+		
 		// DB 조회 - select
+		UserEntity user = userBO.getUserEntityByLoginId(loginId);
 		
 		Map<String, Object> result = new HashMap<>();
-		result.put("code", 200);
-		result.put("is_duplicated_id", true);
+		
+		if (user != null) {
+			result.put("code", 200);
+			result.put("is_duplicated_id", true);
+		} else {
+			result.put("code", 200);
+			result.put("is_duplicated_id", false);
+		}
 		
 		return result;
 		
 	}
+	
+	@PostMapping("/sign-up")
+	public Map<String, Object> signUp(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("email") String email) {
+		
+		// md5 알고리즘  => password hashing
+		// ex)  aaaa => 74b8733745420d4d33f80c4663dc5e5
+		String hashedPassword = EncryptUtils.md5(password);
+		
+		// db insert
+		Integer userId = userBO.addUser(loginId, hashedPassword, name, email);
+		
+		// 응답값
+		Map<String, Object> result = new HashMap<>();
+		if (userId != null) {
+			result.put("code", 200);
+			result.put("result", "성공");
+		} else {
+			result.put("code", 500);
+			result.put("error_message", "회원 가입에 실패했습니다.");
+		}
+				
+		return result;
+		
+	}
+	
+	@PostMapping("/sign-in")
+	public Map<String, Object> signIn(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password) {
+		
+		// 비밀번호 hashing - md5
+		String hashedPassword = EncryptUtils.md5(password);
+		
+		// db 조회(loginId, 해싱된 비밀번호) => UserEntity
+		
+		// 응답값
+		Map<String, Object> result = new HashMap<>();
+		
+		result.put("code", 200);
+		result.put("result", "성공");
+		
+		return result;
+		
+	}
+	
+	
+	
+	
 	
 }
