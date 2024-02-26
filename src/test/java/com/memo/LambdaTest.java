f5749052f0b4048a9b8e735c13151648141796f3
megastudyDESKTOP30 <test@test.com>
2024-02-26 18:42:47 +0900

    2/26

--- src/test/java/com/memo/LambdaTest.java
+++ src/test/java/com/memo/LambdaTest.java
@@ -1,10 +1,13 @@
 package com.memo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -31,7 +34,7 @@ public class LambdaTest {
 		log.info(fruits.toString());
 	}
 	
-	@Test
+	//@Test
 	void 메소드레퍼런스() {
 		List<String> fruits = List.of("apple", "banana", "cherry");
 		fruits = fruits
@@ -41,6 +44,35 @@ public class LambdaTest {
 		
 		log.info(fruits.toString());
 
+	}
+	
+	@Test
+	void 람다_메소드레퍼런스() {
+		List<Person> personList = new ArrayList<>();
+		personList.add(new Person("신보람", 30));
+		personList.add(new Person("김종휘", 28));
+		
+		// 객체 안에 있는 메소드 호출
+		personList.forEach(p -> p.printInfo());   // 람다
+		personList.forEach(Person::printInfo);    // 메소드 레퍼런스
+		
+		// 객체를 println으로 출력
+		personList.forEach(p -> System.out.println(p));  // 람다
+		personList.forEach(System.out::println);  // 메소드 레퍼런스
+		
+	}
+	
+	
+	@ToString
+	@AllArgsConstructor
+	class Person {
+		private String name;
+		private int age;
+		
+		public void printInfo() {
+			log.info("### " + this);
+		}
+		
 	}
 	
 }
